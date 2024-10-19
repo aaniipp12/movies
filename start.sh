65347f887e1c19ccc92e22f1db906b55131dbc00
aaniipp12 <hanifluthfie5@gmail.com>
2024-10-19 10:57:12 +0700

    Update start.sh and requirements.txt to fix environment issues

--- start.sh
+++ start.sh
@@ -4,24 +4,15 @@ export PYTHONUNBUFFERED=true
 
 VIRTUALENV=.data/venv
 
-# Periksa apakah python yang digunakan adalah python 3.9
-if ! python3 --version | grep -q "3.9"; then
-  echo "Python 3.9 is required. Please check your runtime environment."
-  exit 1
-fi
-
-# Buat virtual environment jika belum ada
 if [ ! -d $VIRTUALENV ]; then
   python3 -m venv $VIRTUALENV
 fi
 
-# Install pip jika belum ada
 if [ ! -f $VIRTUALENV/bin/pip ]; then
-  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python3
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/pip/3.7/get-pip.py | $VIRTUALENV/bin/python
 fi
 
-# Install semua dependensi di requirements.txt
 $VIRTUALENV/bin/pip install -r requirements.txt
 
-# Jalankan aplikasi
 $VIRTUALENV/bin/python3 app.py
+Footer
